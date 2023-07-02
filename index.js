396961da6e75116f9dddbed4f0d6f5152caf49aa
Anna Cole <acrrj123@gmail.com>
2023-07-01 19:53:43 -0500

    Lab done

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const printCard = function () {
   console.log(this.insideContent);
   // Wow! Elegant! And notice the arrow function's `this` is the same `this`
   // that printCard has; specifically, the `thisArg` that was passed to it
-  this.signatories.forEach((signatory) =>
+  this.signatories.forEach(signatory =>
     console.log(`${this.closing[signatory]}, ${signatory}`)
   );
 };
